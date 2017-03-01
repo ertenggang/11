dc7ab4568bd0ffa8d25a45a9d42e003b761d9258
Wenjing Zhuang <wenjing_zhuang@htc.com>
2017-03-01 12:27:00 +0800

    modify error control to providing more information.

--- api.py
+++ api.py
@@ -16,22 +16,55 @@ app = Flask(__name__)
 #   else:
 #     return 'hello world'
 
+def enum(**enums):
+    return type('Enum', (), enums)
+
+GET_IMAGE_STATUS = enum(EXCEPTION=1, FORMATERROR=2, OK=3)
+
 def get_image(murl):
-  req = urllib2.Request(murl)
-  req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36')
-  resp = urllib2.urlopen(req)
+  try:
+    req = urllib2.Request(murl)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36')
+    resp = urllib2.urlopen(req)
+  except:
+    return (GET_IMAGE_STATUS.EXCEPTION, None)
   m = resp.read()
   m = np.fromstring(m, np.uint8)
   m = cv2.imdecode(m, cv2.IMREAD_GRAYSCALE)
-  return m
+  if m is None:
+    return (GET_IMAGE_STATUS.FORMATERROR, m)
+  else:
+    return  (GET_IMAGE_STATUS.OK, m)
 
 @app.route('/image_match_api/', methods=['POST','GET'])
 def image_match_api():
+  param_num = 2
+  error_info = []
+  imgs = [[]]*param_num
+  for i in range(param_num):
+    url = request.args.get('m%d'%(i+1))
+    (status, imgs[i]) = get_image(url)
+    if status == GET_IMAGE_STATUS.EXCEPTION:
+      error_info.append({'url':url, 'error': 'Exception raised during opening url.', 'info':'Invalid URL'})
+    elif status == GET_IMAGE_STATUS.FORMATERROR:
+      error_info.append({'url': url, 'error': 'Cannot decode image data', 'info':'Unsupported format or the access if refused.'})
+    elif not status == GET_IMAGE_STATUS.OK:
+      error_info.append({'url':url, 'error':'Unkown error.', 'info':''})
+
+  if len(error_info) <= 0:
+    [result, score, score_type] = image_match(imgs[0], imgs[1])
+    return jsonify({'result': result, 'score':score, 'score_type':score_type})
+  else:
+    return jsonify({'error': 'Fail to fetch images.', 'error_info':error_info, 'error_code':2101})
+
+
+
+
   m1_url = request.args.get('m1')
   m2_url = request.args.get('m2')
 
-  m1 = get_image(m1_url)
-  m2 = get_image(m2_url)
+  (status1, m1) = get_image(m1_url)
+  (status2, m2) = get_image(m2_url)
 
   if m1 is None or m2 is None:
     check_params = {}
