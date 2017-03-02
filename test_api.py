666a7d58b26ab1c08e39444c9be4e05071d1518f
Wenjing Zhuang <wenjing_zhuang@htc.com>
2017-03-02 15:32:10 +0800

    fix bugs

--- test_api.py
+++ test_api.py
@@ -12,14 +12,11 @@ m2 = 'world'
 
 test_data = {'m1':m1, 'm2':m2}
 test_data_urlencode = urllib.urlencode(test_data)
-requrl = 'http://127.0.0.1/5000/image_match_api/'
-headers = {  
-  'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
-}  
-req = urllib2.Request(url=requrl, data=test_data_urlencode, headers = headers)
+requrl = 'http://127.0.0.1:5001/image_match_api/'
+req = urllib2.Request(url=requrl, data=test_data_urlencode)
 print test_data_urlencode
 
-req.add_header("Content-Type", "application/x-www-form-urlencoded")
+# req.add_header("Content-Type", "application/x-www-form-urlencoded")
 
 res_data = urllib2.urlopen(req)
 res = res_data.read()
